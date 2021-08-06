633607bb3ee95fc482302d88c59d72934bcd0e69
akizuki <penpal.pfm@gmail.com>
2021-08-06 20:45:06 +0900

    ファイル構成変更10

--- vue/memo-minigame.vue
+++ vue/memo-minigame.vue
@@ -11,6 +11,15 @@
 			<dt><a href="https://icolor.jp/minigame/" target="_blank">テンプレ・ゲームをカスタマイズして販売</a></dt>
 		</dl>
 
+		<h4>■ ゲーム素材</h4>
+		<dl>
+			<dt><a href="https://biz.moneyforward.com/blog/21173/" target="_blank">【要保存】アプリ開発の初心者必見！アイコンやキャラクターの制作に役立つ、フリー素材サイト20選</a></dt>
+			<dt><a href="http://yms.main.jp/index.html" target="_blank">■ ドット絵世界</a></dt>
+			<dt><a href="https://sites.google.com/site/closetvx/" target="_blank">■ CLOSET</a></dt>
+			<dt><a href="https://qiita.com/TD12734/items/5dc8068732f94452efe2" target="_blank">■ 無料で使えるゲーム素材公開サイト まとめ</a></dt>
+			<dt><a href="https://kenney.nl/assets" target="_blank">■ Kenny（ココイイ！）</a></dt>
+		</dl>
+
 <pre><code>
 </code></pre>
 
